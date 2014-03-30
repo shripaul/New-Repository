f32c3321bac4126b4e008de1c99a46b074015135
shripaul <shripaul5@gmail.com>
2014-03-30 10:47:21 -0400

    java

---
+++ paul/src/paul/mango.java
@@ -0,0 +1,66 @@
+package paul;
+
+import java.util.Scanner;
+public class mango {
+	public static void main(String args[]){
+	
+    	int  counter = 0;
+		while (counter < 10){
+			System.out.println(counter);
+			counter++;
+		}
+		
+	}
+	{
+		int boy, girl;
+		boy = 18;
+		girl = 40;
+		
+		if(boy > 10 && girl < 60){
+			System.out.println("you can enter");
+		}else{
+			System.out.println("you can not enter");
+		}
+		
+		int test = 6;
+		if (test == 9){
+			System.out.println("yes");
+		}else{
+			System.out.println("this is else");
+				
+			}
+		}
+				
+		Scanner paul = new Scanner(System.in);{
+		
+		
+		int man, women, people;
+		man = 12;
+		women = 3;
+		people = man + women;
+		System.out.println(people);
+	
+		double fnum,snum, answer;
+		System.out.println("Enter first name: ");
+		fnum = paul.nextDouble();
+		System.out.println("Enter secound num");
+		snum = paul.nextDouble();
+		answer = fnum + snum;
+		System.out.println(answer);
+		
+		System.out.println(paul.nextLine());
+		
+		System.out.println("Hello google");
+		System.out.println("Hello Bangladesh");
+		
+		/*double tuna;
+		tuna = 8.18;
+		System.out.print("I want ");
+		System.out.print(tuna);
+		System.out.println(" Double");
+		System.out.print("apples");*/
+		
+
+	}
+
+}
